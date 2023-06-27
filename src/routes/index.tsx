39a038d3083dca95f2618c77f70f4f27206e4019
yemao8972 <137700665+yemao8972@users.noreply.github.com>
2023-06-27 20:17:30 +0800

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,16 @@
 import { Title } from "solid-start";
-import Counter from "~/components/Counter";
-
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
-      <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
+      <Title>PRIVACY POLICY</Title>
+	  <h3>Doomsday Flying Claw</h3>
+	  <h3>PRIVACY POLICY</h3>
+		<p>
+		This is a casual single player game that does not collect personal information from users<br/><br/>
+		
+		You can contact us via email: icrenzoqxyu@gmail.com<br/><br/>
+
+		</p>
     </main>
   );
 }
